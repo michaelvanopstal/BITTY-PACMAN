dec3d1acc7706f44a4dc896aa7b425a7a08cf8b3
vkjhbjk <44727975+michaelvanopstal@users.noreply.github.com>
2025-12-10 22:21:40 +0100

    Update game.js

--- game.js
+++ game.js
@@ -243,9 +243,9 @@ let allPowerDotsUsed = false;  // wordt true na de allerlaatste 'O'
 
 const cherryIconConfig = {
   enabled: true,
-  x: 780,    // positie op het scherm (px)
+  x: 720,    // positie op het scherm (px)
   y: 305,    // naast of onder je lives, pas zelf aan
-  scale: 1.2 // 1.0 = normaal, 1.2 = iets groter
+  scale: 0.8 // 1.0 = normaal, 1.2 = iets groter
 };
 
 function drawCherryIcon() {
