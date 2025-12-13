28f48c94aabc1893e40203b47040540c9ea97377
vkjhbjk <44727975+michaelvanopstal@users.noreply.github.com>
2025-12-13 21:30:46 +0100

    Update game.js

--- game.js
+++ game.js
@@ -153,7 +153,7 @@ bananaImg.src = "banaan.png";
 let banana = null;
 let bananasSpawned = 0;
 let nextBananaThresholds = [60, 150, 260]; // voorbeeld ritme, pas aan
-const bananaIconConfig = { enabled: true, x: 740, y: 303, scale: 0.8 };
+const bananaIconConfig = { enabled: true, x: 680, y: 430, scale: 0.8 };
 
 // ─────────────────────────────────────────────
 // 🍐 PEER SYSTEM (LEVEL 3 ONLY)
@@ -169,7 +169,7 @@ let pearsSpawned = 0;  // exact 3 per level
 let nextPearThresholds = [90, 190, 280];
 
 // HUD icoon (naast banaan)
-const pearIconConfig = { enabled: true, x: 680, y: 450, scale: 0.8 };
+const pearIconConfig = { enabled: true, x: 680, y: 410, scale: 1.0 };
 
 
 // Fine-tune bullet X binnen de lane (pixels, positief = naar rechts)
