3ddb5405034c3e70b9e46425bd8362a0018a834d
vkjhbjk <44727975+michaelvanopstal@users.noreply.github.com>
2025-12-23 18:32:37 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2636,12 +2636,15 @@ function updatePlayer() {
 function onAllDotsCleared() {
   console.log("✨ All dots cleared!");
 
-  if (currentLevel === 1) {
+   if (currentLevel === 1) {
     currentLevel = 2;
     readyLabel = "LEVEL 2";
   } else if (currentLevel === 2) {
     currentLevel = 3;
     readyLabel = "LEVEL 3";
+  } else if (currentLevel === 3) {
+    currentLevel = 4;
+    readyLabel = "LEVEL 4";
   } else {
     console.log("🎉 Alle levels klaar!");
     return;
